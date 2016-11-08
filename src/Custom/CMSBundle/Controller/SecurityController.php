8226dfb6d610af3c7617a8a8b8008328efd413b4
ravelmello <ravelmello@hotmail.com>
2016-11-08 10:06:24 -0300

    correção do caminho para o template do twig

--- src/Custom/CMSBundle/Controller/SecurityController.php
+++ src/Custom/CMSBundle/Controller/SecurityController.php
@@ -15,7 +15,7 @@ class SecurityController extends Controller
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render(
-            'C:\xampp\htdocs\php_projects\cms\src\Custom\CMSBundle\Resources\views\login.html.twig',
+            '@CustomCMS/login.html.twig',
             array(
                 // last username entered by the user
                 'last_username' => $lastUsername,
